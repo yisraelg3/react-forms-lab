83de52c2894f71385984c74ea24b4df85a5c2e97
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-23 22:48:26 -0400

    commit tree 0f69e5d6832cbadbb7346feef4a03c93dd4cc82f

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+      //counter: 0
+    };
+  }
+
+  handleChange = (event) => {
+    //console.log(event.target.value.length )
+    
+    this.setState({
+      message: event.target.value
+      //counter: newCounter
+    })
   }
 
   render() {
+    //console.log(this.state.message)
+    let newCounter = this.props.maxChars - this.state.message.length 
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+       {newCounter}
       </div>
     );
   }
